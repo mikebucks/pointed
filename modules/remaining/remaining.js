4cfa5a9e226c8de31f12e0872ade8506d0601ffa
mikebucks <dangerbucks@gmail.com>
2013-10-10 15:38:41 -0700

    adds remaining module and gitignore

---
+++ modules/remaining/remaining.js
@@ -0,0 +1,26 @@
+/*!
+ * @directive `remaining`
+ * @description character count for text field
+ */
+
+angular.module('pt.remaining', []).directive('remaining', function () {
+  return {
+    template: "{{remaining}} characters",
+    scope: {
+      maxLen: '@max',
+      model: '=ngModel'
+    },
+    link: function(scope, elem, attrs) {
+      scope.$watch('model', function(val) {
+        if (val === null || val === undefined) return;
+
+        var maxLen = parseInt(scope.maxLen, 10);
+        var remaining = maxLen - val.length;
+        if (remaining < 0)
+          scope.model = val.substr(0, maxLen);
+        else
+          scope.remaining = remaining; 
+      });
+    }
+  };
+});
