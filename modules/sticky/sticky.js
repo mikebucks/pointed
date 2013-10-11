6ebf305e1bc2573602a3a74331e9cf3cd5a662b8
mikebucks <dangerbucks@gmail.com>
2013-10-11 11:06:53 -0700

    remove css dependency from sticky.js

--- modules/sticky/sticky.js
+++ modules/sticky/sticky.js
@@ -24,39 +24,11 @@ angular.module('pt.sticky', []).directive('ptSticky',
 
         $rootScope.$on('$routeChangeSuccess', watchWindow);
 
-        // Insert dummy elements to preserve layout after original becomes fixed
+        // Insert dummy element to preserve layout after original becomes fixed
         el.clone().empty().addClass('dummy-sticky').addClass('dummy-'+attr.stickyIndex).insertBefore(el);
         $dummy_sticky = $('.dummy-'+attr.stickyIndex);
 
-        var watchWindow = function() {
-          if ($('.dummy-sticky').length >= 1) {
-            $window.scroll(function() { scrolling = true; });
-            $window.resize(function() { resizing = true; });
-          } else {
-            $window.unbind('scroll');
-            $window.unbind('resize');
-          }
-        };
-
-        var addSticky = function() {
-          $dummy_sticky.css({
-            'width'  : el_width,
-            'height' : el_height
-          });
-          el.addClass('fixed').css({
-            'top'     : stickyOffset,
-            'left'    : el_position.left,
-            'width'   : el_width,
-            'z-index' : attr.stickyIndex+5 || 5
-          });
-        };
-
-        var removeSticky = function() {
-          el.removeClass('fixed').removeAttr('style');
-          $dummy_sticky.css({ 'width' : 0, 'height': 0 });
-        };
-
-        // Scroll/Resize event runs every 50 ms. Any slower and the stickiness feels clunky.
+        // Scroll and Resize event runs every 50 ms. Any slower and the stickiness feels clunky.
         setInterval(function() {
           if (scrolling || resizing) {
             el_position = $dummy_sticky.offset();
@@ -66,16 +38,13 @@ angular.module('pt.sticky', []).directive('ptSticky',
             resizing = false;
 
             try {
-              // no sticky on mobile
-              if ($window.width() <= minWidth) {
-                removeSticky();
+              if ($window.width() <= minWidth) { // no sticky on mobile
+                unfixEl();
                 return;
-              // wide screen sticky
               } else if ($window.scrollTop()+stickyOffset >= el_position.top && el.height() <= $window.height()) {
-                addSticky();
-              // kill sticky
+                fixEl();
               } else {
-                removeSticky();
+                unfixEl();
               }
             } catch(err) {
               console.log('Pointed Sticky Directive Error: ' + err);
@@ -83,6 +52,35 @@ angular.module('pt.sticky', []).directive('ptSticky',
             }
           }
         }, 50);
+
+        var watchWindow = function() {
+          if ($('.dummy-sticky').length >= 1) {
+            $window.scroll(function() { scrolling = true; });
+            $window.resize(function() { resizing = true; });
+          } else {
+            $window.unbind('scroll resize');
+            // $window.unbind('');
+          }
+        };
+
+        var fixEl = function() {
+          $dummy_sticky.css({
+            'width'  : el_width,
+            'height' : el_height
+          });
+          el.css({
+            'position': 'fixed',
+            'top'     : stickyOffset,
+            'left'    : el_position.left,
+            'width'   : el_width,
+            'z-index' : attr.stickyIndex+5 || 5
+          });
+        };
+
+        var unfixEl = function() {
+          el.removeClass('fixed').removeAttr('style');
+          $dummy_sticky.css({ 'width' : 0, 'height': 0 });
+        };
 
         watchWindow();
       }
